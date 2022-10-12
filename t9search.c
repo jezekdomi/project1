1a59aeac048db8313ae3691f56332957103014a2
jezekdomi <d.dominikjezek2@seznam.cz>
2022-10-12 20:39:50 +0200

    t9search made

--- t9search.c
+++ t9search.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 // 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
+
+#define TRUE 1
+#define FALSE 0
+
 int main(int argc, char const *argv[])
 {
     printf("%s\n", argv[1]);
