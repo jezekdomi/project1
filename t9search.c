200b33f87700e8933df640266277d17ed3157b4c
jezekdomi <d.dominikjezek2@seznam.cz>
2022-10-13 10:48:48 +0200

    Ubuntu installed to Win

--- t9search.c
+++ t9search.c
@@ -7,8 +7,27 @@
 #define TRUE 1
 #define FALSE 0
 
+void display_by_filter(char const *array)
+{
+    int i = 0;
+    while (array[i] != 0)
+    {
+        printf("%c", array[i]);
+        i++;
+    }        
+}
+
 int main(int argc, char const *argv[])
 {
-    printf("%s\n", argv[1]);
+    if (argc == 2)
+    {
+        printf("Contacts filtered by: \'%s\' should be displayed!\n", argv[1]);
+        display_by_filter(argv[1]);
+    }
+    else
+    {        
+        printf("Too many parameters entered!\n");
+        return 0;
+    }    
     return 0;
-}
+}   
