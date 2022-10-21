b76362035e61e9d945eb9fcb9d4c2690709d10e0
jezekdomi <d.dominikjezek@seznam.cz>
2022-10-21 04:32:03 +0200

    Rework

--- t9search.c
+++ t9search.c
@@ -1,104 +1,67 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-struct Contact
+/* VARIABLES */
+
+#define MAX_CONTACTS 42
+#define MAX_LINE_LENGTH 100
+
+typedef struct Contact
 {
-    char name[100]; 
-    char phone[100];    
-};
+    char name[MAX_LINE_LENGTH];
+    char phone[MAX_LINE_LENGTH];
+} 
+Contact;
 
-const int keyboard[9][5] = {
-    {'0', '+'},
-    {'2', 'a', 'b', 'c'},
-    {'3', 'd', 'e', 'f'},
-    {'4', 'g', 'h', 'i'},
-    {'5', 'j', 'k', 'l'},
-    {'6', 'm', 'n', 'o'},
-    {'7', 'p', 'q', 'r', 's'},
-    {'8', 't', 'u', 'v'},
-    {'9', 'x', 'y', 'z'}
-};
+Contact contact[MAX_CONTACTS];
+int line;
 
-int main(int argc, char const *argv[])
-{    
-    if (argc > 2)
+/* --------- */
+
+/* FUNCTIONS */
+
+void read_stdin()
+{
+    char c;
+    line = 1;
+    char name_or_phone[MAX_LINE_LENGTH];
+
+    while (!feof(stdin) && line / 2 <= MAX_CONTACTS)
     {
-        fprintf(stderr, "Too many arguments!\n");
-        return 1;
-    }
-    else if (argc == 1)
-    {
-        printf("Whole list be displayed!\n");
-    }    
-    else
-    {
-        printf("List filtered by: %s, shall be displayed!\n", argv[1]);
-    }
-    
-    int c;
-    if ((c = getchar()) == EOF)
-    {
-        fprintf(stderr, "No contacts found!\n");
-        return 1;
-    }
-
-    // cti radek a zapis ho do struct kontakt
-    unsigned current_line = 1;
-    char line[100];
-    int i_contact;
-    struct Contact contact[42];
-    do
-    {        
-        int i = 0;
-        do {
-            line[i] = tolower(c);
-            i++;
-        } 
-        while ((c = getchar()) != '\r' && c != EOF && i < 100);
-        line[i] = '\0';
-
-        i_contact = current_line % 2 == 1 ? current_line / 2 : current_line / 2 - 1;
-        strcpy(current_line % 2 == 1 ? contact[i_contact].name : contact[i_contact].phone, line);
-        
-        c = getchar(); // flush '\n' from stdin
-        current_line++;
-
-        // if (current_line % 2 == 0) printf("%s, %s\n", contact[i_contact].name, contact[i_contact].phone);
-    }    
-    while ((c = getchar()) != EOF && current_line <= 84);
-    
-    int contacts_count = i_contact + 1;
-    i_contact = 0;
-    while (i_contact < contacts_count)
-    {
-        int num_match_count = 0;
-        int i_prev_matched = -1;
-        int i_char_in_phone;
-        int i_query = 0;
-
-        // query phone number
-        while (i_query < (int)strlen(argv[1]))
+        for (int i = 0; i < MAX_LINE_LENGTH; i++)
         {
-            i_char_in_phone = i_prev_matched + 1;
-
-            while (argv[1][i_query] != contact[i_contact].phone[i_char_in_phone] && contact[i_contact].phone[i_char_in_phone] != '\0')
+            // making sure that number is going to be number and name name, with its valid possibilities
+            if ((((c = tolower(getchar())) == '.' || isalpha(c)) && (line % 2 == 1)) || isdigit(c) || c == ' ' || c == '\n' || c == '\r')
             {
-                i_char_in_phone++;
+                if (c == '.' || c == ' ' || isalnum(c))
+                {
+                    name_or_phone[i] = c;
+                }
+                else if (c != '\n')
+                {
+                    name_or_phone[i] = '\0';
+                }
+                else break;
             }
-
-            if (argv[1][i_query] == contact[i_contact].phone[i_char_in_phone] && i_prev_matched < i_char_in_phone)
+            else
             {
-                i_prev_matched = i_char_in_phone;
-                num_match_count++;
-            }            
-            i_query++;
-
-            if (i_query == num_match_count && i_query == (int)strlen(argv[1]))
-            {
-                printf("%s, %s\n", contact[i_contact].name, contact[i_contact].phone);
+                fprintf(stderr, "Unexpected input data on line: %d!\n", line);
+                exit(0);
             }
         }
-        i_contact++;        
+        
+        strcpy((line % 2) == 1 ? contact[line / 2].name : contact[line / 2 - 1].phone, name_or_phone);
+        line++;
     }
+}
+
+/* --------- */
+
+int main(int argc, char const *argv[])
+{
+    read_stdin();
+    return 0;
 }
